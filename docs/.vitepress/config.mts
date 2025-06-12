ccd061f6afffeaeddb5566826bcb7966e54b6f8f
张峰 <fecn1987@gmail.com>
2025-06-12 12:08:48 +0800

    first commit

---
+++ docs/.vitepress/config.mts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "zf2014",
+  description: "zf2014"
+})
---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  import { useData } from 'vitepress';
+
+  interface Frontmatter {
+    home?: boolean;
+  }
+
+  interface SiteData {
+    title: string;
+    description: string;
+  }
+
+  // https://vitepress.dev/reference/runtime-api#usedata
+  const { site, frontmatter } = useData<{
+    frontmatter: Frontmatter;
+    site: SiteData;
+  }>();
+</script>
+
+<template>
+  <div v-if="frontmatter?.home">
+    <h1>{{ site.title }}</h1>
+    <p>{{ site.description }}</p>
+    <ul>
+      <li>
+        <a href="/markdown-examples.html">Markdown Examples</a>
+      </li>
+      <li><a href="/api-examples.html">API Examples</a></li>
+    </ul>
+  </div>
+  <div v-else>
+    <a href="/">Home</a>
+    <Content />
+  </div>
+</template>
